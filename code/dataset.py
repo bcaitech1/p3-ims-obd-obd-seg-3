c353383575cefc1d3a1c517f423c705f18015335
GJ <lgj9172@gmail.com>
2021-04-27 20:05:52 +0900

    refactor: remove load_category_names method

--- code/dataset.py
+++ code/dataset.py
@@ -12,53 +12,6 @@ import cv2
 import os
 
 dataset_path = '/opt/ml/input/data'
-def load_category_names():
-    dataset_path = '/opt/ml/input/data'
-    anns_file_path = dataset_path + '/' + 'train.json'
-
-    # Read annotations
-    with open(anns_file_path, 'r') as f:
-        dataset = json.loads(f.read())
-
-    categories = dataset['categories']
-    anns = dataset['annotations']
-    imgs = dataset['images']
-    nr_cats = len(categories)
-
-    # Load categories and super categories
-    cat_names = []
-    super_cat_names = []
-    super_cat_ids = {}
-    super_cat_last_name = ''
-    nr_super_cats = 0
-    for cat_it in categories:
-        cat_names.append(cat_it['name'])
-        super_cat_name = cat_it['supercategory']
-        # Adding new supercat
-        if super_cat_name != super_cat_last_name:
-            super_cat_names.append(super_cat_name)
-            super_cat_ids[super_cat_name] = nr_super_cats
-            super_cat_last_name = super_cat_name
-            nr_super_cats += 1
-
-    # Count annotations
-    cat_histogram = np.zeros(nr_cats,dtype=int)
-    for ann in anns:
-        cat_histogram[ann['category_id']] += 1
-
-    # Convert to DataFrame
-    df = pd.DataFrame({'Categories': cat_names, 'Number of annotations': cat_histogram})  # num of 저거 빼도 되지 않나?
-    df = df.sort_values('Number of annotations', 0, False)
-    # category labeling 
-    sorted_temp_df = df.sort_index()
-
-    # background = 0 에 해당되는 label 추가 후 기존들을 모두 label + 1 로 설정
-    sorted_df = pd.DataFrame(["Backgroud"], columns = ["Categories"])
-    sorted_df = sorted_df.append(sorted_temp_df, ignore_index=True)
-
-    category_names = list(sorted_df.Categories)
-    return category_names
-
 
 def get_classname(classID, cats):
     for i in range(len(cats)):
