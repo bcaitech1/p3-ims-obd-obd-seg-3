d569f9f91e8c8186340e0a31c6cdd2b73f9a4bb8
sukmin <mellonggo@naver.com>
2021-04-29 08:39:22 +0000

    feat: [utils] seed_everthing

--- code/utils.py
+++ code/utils.py
@@ -2,6 +2,7 @@
 import numpy as np
 import torch
 import random
+import os
 
 
 def _fast_hist(label_true, label_pred, n_class):
@@ -38,6 +39,7 @@ def label_accuracy_score(label_trues, label_preds, n_class):
 def seed_everything(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
     torch.cuda.manual_seed_all(seed)  # if use multi-GPU
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
